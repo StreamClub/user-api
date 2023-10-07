fafb7002c365eabfeb6ea1f2cddd41abaa4546a3
scapelli <scapelli@fi.uba.ar>
2023-10-07 17:40:32 -0300

    database integration

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const pg = require('pg')
 
+dotenv.config()
+
+const pool = new pg.Pool({
+    connectionString: process.env.USER_DB_URL,
+    ssl: true // VERIFICAR: revisar con este comando funciona en localhost, pero en la rama deployada de render puede que no
+})
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Stream Club!')
+app.get('/', async (req, res) => {
+    const result = await pool.query('select now()')
+    const hora = result.rows[0]
+    return res.json(hora)
+})
+
+app.get('/users', async (req, res) => {
+    const result = await pool.query('select * from usuarios')
+    const users = result.rows
+    res.send(users)
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
