f03ef4847812cb309463cb20d3276e95337e514c
scapelli <scapelli@fi.uba.ar>
2023-10-08 00:16:16 -0300

    docker-compose now initialize both db and uapi service, Sequelize incorporated in app.ts and healthcheck incorporated in docker-compose

--- src/app.ts
+++ src/app.ts
@@ -2,48 +2,62 @@ const express = require('express')
 const dotenv = require('dotenv')
 const pg = require('pg')
 
-// import { dal.UserDal } from './dal';
-const dal = require('./dal')
 dotenv.config()
 
-const pool = new pg.Pool({
-    connectionString: process.env.DB_URL,
-    // ssl: true 
-    // VERIFICAR: revisar con este comando funciona en localhost, pero en la rama deployada de render puede que no
-})
+
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+// si se ejecuta localmente con "npm start" se usa esta linea
+// const sequelize = new Sequelize('postgres://StreamClub:DatosauriosFiuba@localhost/user_db')
+
+// si se ejecuta con "docker-compose up" se usa esta linea
+const sequelize = new Sequelize('postgres://StreamClub:DatosauriosFiuba@user_db_service/user_db')
+// const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+    },
+    displayname: {
+        type: DataTypes.STRING
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING
+    }
+});
+
 const app = express()
 const port = 8080
 
-// app.get('/', async (req, res) => {
-//     const result = await pool.query('select now()')
-//     const hora = result.rows[0]
-//     return res.json(hora)
-// })
+app.get('/', async (req, res) => {
+    res.status(200).json({ status: 'UAPI listening in port 8080' });
+})
 
 app.get('/users', async (req, res) => {
-    const users = await dal.UserDal.findAll();
+    const users = await User.findAll();
     res.send(users)
 })
 
-
-
-
-app.get('/users/create', async (req, res) => {
-
-    const jane = await dal.UserDal.create({
-        username: 'usuario1',
-        displayName: 'displayName1',
-        email: 'example@example.com',
-        password: '123456',
-
-    });
-    
-
-    res.send('Creado!')
+app.get('/drop_users', async (req, res) => {
+    await User.drop();
+    await User.sync({ force: true });
+    res.send('Tabla de usuarios dropeada!')
 })
 
+app.get('/create_user', async (req, res) => {
+    const _ = await User.create({
+        username: 'user1',
+        displayName: 'displayName1',
+        email: 'example@example.com',
+        password: 'password1'
+    });
+    res.send('user1 creado!')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
--- src/dal/users/userDal.ts
+++ src/dal/users/userDal.ts
@@ -1,6 +1,6 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-const sequelize = new Sequelize('sqlite::memory:');
+const sequelize = new Sequelize('postgres://StreamClub:DatosauriosFiuba@user_db_service/user_db');
 export const UserDal = sequelize.define('User', {
   userName: DataTypes.STRING,
   displayName: DataTypes.STRING,
