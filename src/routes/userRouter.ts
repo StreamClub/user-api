0fb53b30e9e7774a1cec2c48fd2830ba3a446ab8
Hugo Larrea <hugo.larrea@creativecoefficient.net>
2023-10-10 19:55:59 -0300

    Remove Unused imports and comented code

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -3,12 +3,10 @@ import {
     handleRequest,
     validateSchema,
 } from "@middlewares";
-import { Request } from "@models";
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 import { LoginSchema, RegisterUserSchema } from "@dtos";
 import { userController } from "@controllers";
-import { config } from "@config";
 
 export function UserRouter() {
     const router = Router();
@@ -33,45 +31,3 @@ export function UserRouter() {
 
     return router;
 }
-
-// const { Sequelize, Model, DataTypes } = require("sequelize");
-
-// // si se ejecuta localmente con "npm start" se usa esta linea
-// const sequelize = new Sequelize(config.dbUrl);
-
-// // si se ejecuta con "docker-compose up" se usa esta linea
-// const sequelize = new Sequelize(config.dbUrl);
-// // const sequelize = new Sequelize(config.dbUrl);
-
-// const User = sequelize.define('User', {
-//     username: {
-//         type: DataTypes.STRING,
-//     },
-//     displayname: {
-//         type: DataTypes.STRING
-//     },
-//     email: {
-//         type: DataTypes.STRING,
-//         unique: true
-//     },
-//     password: {
-//         type: DataTypes.STRING
-//     }
-// });
-
-// const app = express()
-
-// app.get('/users', async (req, res) => {
-//     const users = await User.findAll();
-//     res.send(users)
-// })
-
-// app.get('/create_user', async (req, res) => {
-//     const _ = await User.create({
-//         username: 'user1',
-//         displayName: 'displayName1',
-//         email: 'example@example.com',
-//         password: 'password1'
-//     });
-//     res.send('user1 creado!')
-// })
