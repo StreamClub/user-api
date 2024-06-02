9e59695078b28b14a3c90c909a1a628be73452d6
Hugo Larrea <hlarrea@fi.uba.ar>
2024-06-02 15:52:56 -0300

    reduce amount of levels

--- src/config/levels.ts
+++ src/config/levels.ts
@@ -3,5 +3,4 @@ export const LEVELS = [
     { name: "Reseñador Aficionado", threshold: 1000 }, // 2 reviews
     { name: "Analista Experto", threshold: 5000 }, // 10 reviews
     { name: "Crítico Conocedor", threshold: 10000 }, // 20 reviews
-    { name: "Gurú del Entretenimiento", threshold: 20000 }, // 40 reviews
 ]
